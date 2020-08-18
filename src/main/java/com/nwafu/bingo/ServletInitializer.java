b51bd2b6d6eea2adcc900c4a12ff55ebdba3ef16
SilentSherlock <1536141515@qq.com>
2020-08-18 08:37:35 +0800

    initialize the repo

---
+++ src/main/java/com/nwafu/bingo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.nwafu.bingo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BingoApplication.class);
+	}
+
+}
