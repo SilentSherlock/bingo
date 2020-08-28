1a0d607799e2171e40b9fbe0ed5a1f2a43be6d72
lwh147 <wanghaolin147@gmail.com>
2020-08-28 14:44:16 +0800

    pageController
    1.添加pageController
    2.添加所有游戏界面

--- src/main/java/com/nwafu/bingo/control/PageController.java
+++ src/main/java/com/nwafu/bingo/control/PageController.java
@@ -4,15 +4,67 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * Date: 2020/8/22
- * Description: optional describe the class
- */
+ * @Author lwh
+ * @Date 2020/8/25 15:30
+ * @Version 1.0
+ * @ClassName PageController
+ * @Description 返回html的controller
+ **/
 @Controller
+@RequestMapping("page")
 public class PageController {
+    //测试页面
+    @RequestMapping("test")
+    public String test() {
+        return "test";
+    }
 
-    @RequestMapping("/login")
-    public String login() {
-        return "personTest";
+    //静态组件----------------------------
+    @RequestMapping("alert")
+    public String alert() {
+        return "static_components/alert";
+    }
+
+    @RequestMapping("component_index")
+    public String component_index() {
+        return "static_components/component_index";
+    }
+
+    @RequestMapping("component_user_login")
+    public String component_user_login() {
+        return "static_components/component_user_login";
+    }
+
+    @RequestMapping("component_user_register")
+    public String component_user_register() {
+        return "static_components/component_user_register";
+    }
+
+    @RequestMapping("component_loading_animation")
+    public String component_loading_animation() {
+        return "static_components/component_loading_animation";
+    }
+
+    @RequestMapping("component_load_error")
+    public String component_load_error() {
+        return "static_components/component_load_error";
+    }
+
+    @RequestMapping("component_none_result")
+    public String component_none_result() {
+        return "static_components/component_none_result";
+    }
+
+    @RequestMapping("component_all_game")
+    public String component_all_game() {
+        return "static_components/component_all_game";
+    }
+
+
+    //动态组件----------------------------
+    @RequestMapping("component_game_recommendation_card")
+    public String component_game_recommendation_card() {
+        return "dynamic_components/component_game_recommendation_card";
     }
 
 }
